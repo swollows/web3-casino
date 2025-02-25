3f67846508e4e34d4ca5975a9abbc24dae660ed4
swollows <swollows9081@gmail.com>
2025-02-25 20:44:56 +0900

    Add CasinoToken minting & Exchange amongs user

--- src/tokens/CasinoToken.sol
+++ src/tokens/CasinoToken.sol
@@ -1,10 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-import "./ERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ERC20Pausable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract CasinoToken is ERC20 {
-    constructor(string memory _name, string memory _symbol, string memory _version) 
-        ERC20(_name, _symbol, _version)
-    {}
+/// @custom:security-contact jonathan@upside.center
+contract CasinoToken is ERC20, ERC20Permit, ERC20Pausable, ERC20Burnable, Ownable {
+    constructor(address initialOwner, address recipient) 
+        ERC20("Jonathan's Casino Token", "JCT")
+        Ownable(initialOwner)
+        ERC20Permit("Jonathan's Casino Token")
+    {
+        _mint(recipient, 10000000000 * 10 ** decimals());
+    }
+
+        function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20, ERC20Pausable)
+    {
+        super._update(from, to, value);
+    }
 }
