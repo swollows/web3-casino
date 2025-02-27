42494ecb3b5c25447297925001b907da6b8ce92e
swollows <swollows9081@gmail.com>
2025-02-27 22:16:35 +0900

    Add foundry deployment script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,15 +2,20 @@
 pragma solidity ^0.8.10;
 
 import "forge-std/Script.sol";
-import "../src/JonathanCasinoToken.sol";
-import "../src/games/BlackjackGame.sol";
-import "../src/games/CoinTossGame.sol";
-import "../src/games/RouletteGame.sol";
-import "../src/proxy/GameProxy.sol";
+import "../src/token/JonathanCasinoToken.sol";
+
 
 contract Deploy is Script {
     function run() public {
         vm.startBroadcast();
+
+        address owner = msg.sender;
+
+        // 컨트랙트 배포 (배포 시 1 ether를 보내야 함)
+        JonathanCasinoToken token = new JonathanCasinoToken{value: 1 ether}(owner);
+
+        // 컨트랙트 주소 출력
+        console.log("Token deployed at:", address(token));
 
         vm.stopBroadcast();
     }
