b344816c60058d78805198ea6d505af823d2c066
swollows <swollows9081@gmail.com>
2025-02-28 19:18:18 +0900

    Remove All Children Code from GameProxy(it will be worked with only parent code)

--- src/proxy/GameProxy.sol
+++ src/proxy/GameProxy.sol
@@ -3,39 +3,4 @@ pragma solidity ^0.8.10;
 
 import "./ProxyBase.sol";
 
-contract BlackjackProxy is ProxyBase {
-    constructor(address _casinoCounter, address _JCTToken)
-     ProxyBase(_casinoCounter, _JCTToken)
-     {}
-
-    function upgradeDelegate(address newDelegateAddress) public override onlyOwner {
-        require(msg.sender == owner(), "Only owner can upgrade Blackjack contract");
-        implementation = newDelegateAddress;
-    }
-}
-
-contract CoinTossProxy is ProxyBase {
-    constructor(address _casinoCounter, address _JCTToken)
-     ProxyBase(_casinoCounter, _JCTToken)
-     {}
-
-    function upgradeDelegate(address newDelegateAddress) public override onlyOwner {
-        require(msg.sender == owner(), "Only owner can upgrade CoinToss contract");
-
-        console.log("CoinTossProxy implementation:", implementation);
-        implementation = newDelegateAddress;
-
-        console.log("CoinTossProxy implementation:", implementation);
-    }
-}
-
-contract RouletteProxy is ProxyBase {
-    constructor(address _casinoCounter, address _JCTToken)
-     ProxyBase(_casinoCounter, _JCTToken)
-     {}
-
-    function upgradeDelegate(address newDelegateAddress) public override onlyOwner {
-        require(msg.sender == owner(), "Only owner can upgrade Roulette contract");
-        implementation = newDelegateAddress;
-    }
-}
+contract CoinTossProxy is ProxyBase {}
